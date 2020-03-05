8e45089845316214a8e6e62aac371a801e77ce19
Setu Potnis <setupotnis@gmail.com>
2020-03-05 10:10:00 -0500

    added stateless widget and simple text

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(10),
+            height: 100,
+            width: 100,
+            margin: EdgeInsets.symmetric(vertical: 50, horizontal: 10),
+            color: Colors.white,
+            child: Text('hello'),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }

39c65882c3348bff71be0b72a157f9598eada4f4
Setu Potnis <setupotnis@gmail.com>
2020-03-07 11:14:51 -0500

    added ListTile widget and card widget to shorten code

--- lib/main.dart
+++ lib/main.dart
@@ -11,82 +11,79 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.cyan[200],
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('images/SetuPotnisHeadshot.jpg'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/SetuPotnisHeadshot.jpg'),
+              ),
+              Text(
+                'Setu Potnis',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'SOFTWARE DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 25,
+                  color: Colors.cyan[700],
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
                 ),
-                Text(
-                  'Setu Potnis',
-                  style: TextStyle(
-                      fontFamily: 'Pacifico',
-                      fontSize: 40.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.cyan[600],
                 ),
-                Text(
-                  'SOFTWARE DEVELOPER',
-                  style: TextStyle(
-                    fontFamily: 'SourceSansPro',
-                    fontSize: 25,
-                    color: Colors.cyan[400],
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2.5,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.cyan[400],
+                    ),
+                    title: Text(
+                      '+1 647-408-3032',
+                      style: TextStyle(
+                        color: Colors.cyan.shade800,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20,
+                      ),
+                    ),
                   ),
                 ),
-                Container(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  padding: EdgeInsets.all(10),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.phone,
-                        color: Colors.cyan[400],
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.alternate_email,
+                      color: Colors.cyan[400],
+                    ),
+                    title: Text(
+                      'spotnis@uwaterloo.ca',
+                      style: TextStyle(
+                        color: Colors.cyan.shade800,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20,
                       ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        '+1 647-408-3032',
-                        style: TextStyle(
-                            color: Colors.cyan.shade800,
-                            fontFamily: 'SourceSansPro',
-                            fontSize: 20,
-                            letterSpacing: 2.5),
-                      )
-                    ],
+                    ),
                   ),
                 ),
-                Container(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  padding: EdgeInsets.all(10),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.alternate_email,
-                        color: Colors.cyan[400],
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        'spotnis@uwaterloo.ca',
-                        style: TextStyle(
-                            color: Colors.cyan.shade800,
-                            fontFamily: 'SourceSansPro',
-                            fontSize: 20,
-                            letterSpacing: 2.5),
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
